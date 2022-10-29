ba12a1cb460d81dac26d535c3abad8d37ce6fcf8
Manu <manuelsoon@gmail.com>
2022-10-29 18:00:44 +0200

    adding gm

--- gameserver/log/SearchLogs_loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while :;
-do
-java -Dtakipi.name="Logs_Search" -agentlib:TakipiAgent LogsCheck allItems.log 0 -1 274045216 > result.txt 2>&1
-        [ $? -ne 2 ] && break
-        sleep 30;
-done
--- gameserver/log/StartSearchLogs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./SearchLogs_loop.sh &
